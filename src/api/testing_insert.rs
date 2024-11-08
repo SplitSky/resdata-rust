e580991e9848f5ecf717c24fd69da4a875bda0c4
SplitSky <opop.neska60@gmail.com>
2024-11-08 16:27:59 +0000

    rust analyser finally working. Rebound the resource to use rustup instead for source code management

--- src/api/testing_insert.rs
+++ src/api/testing_insert.rs
@@ -1,12 +1,11 @@
-use crate::MyDocument;
+use crate::api::model::MyDocument;
 use actix_web::{post, web, HttpResponse, Responder};
-use mongodb::bson::doc;
+// use mongodb::bson::doc;
 use mongodb::Client;
-use crate::MyDocument
 
 #[post("/insert")]
 pub async fn insert_dataset(client: web::Data<Client>,json_body: web::Json<MyDocument>,) -> impl Responder {
-    let db = client.databset("Your_database_name");
+    let db = client.database("Your_database_name");
     let collection = db.collection::<MyDocument>("my_collection");
     let new_doc = MyDocument{
         id: None,
@@ -14,7 +13,7 @@ pub async fn insert_dataset(client: web::Data<Client>,json_body: web::Json<MyDoc
         description: json_body.description.clone(),
     };
     match collection.insert_one(new_doc, None).await {
-        Ok(result) => HttpResponse::Ok().json(!doc {"inserted_id": result.inserted_id}),
+        Ok(result) => HttpResponse::Ok().json(!doc {"inserted_id": result.inserted_id,}),
         Err(e) => HttpResponse::InternalServerError().body(format!("Failed to insert document: {}", e)),
     }
 }
