6748b8a98f7ed2a4fa4e56fa03e2d842bf5c4a0e
SplitSky <opop.neska60@gmail.com>
2025-05-01 19:28:30 +0100

    Refactor: Added some changes to handle errors. The middleware error handling for fetching the document from mongoDB is not done properly. Unwrap the coursor object and then handle the cases

--- src/middleware/db.rs
+++ src/middleware/db.rs
@@ -1,6 +1,10 @@
 use crate::model::data_structs::Dataset; // , Experiment, Project};
 use futures::TryStreamExt;
-use mongodb::{bson::oid::ObjectId, bson::doc, options::ClientOptions, Client, Collection};
+use mongodb::{
+    Client, Collection,
+    bson::{doc, oid::ObjectId},
+    options::{ClientOptions, TlsOptions},
+};
 
 #[derive(Clone)]
 pub struct Db {
@@ -8,8 +12,13 @@ pub struct Db {
 }
 
 impl Db {
-    pub async fn new(connection_string: &str, database_name: &str, collection_name: &str) -> mongodb::error::Result<Self> {
+    pub async fn new(
+        connection_string: &str,
+        database_name: &str,
+        collection_name: &str,
+    ) -> mongodb::error::Result<Self> {
         let client_options = ClientOptions::parse(connection_string).await?;
+        // client_options.tls = Some(mongodb::options::Tls::Enabled(TlsOptions::default())); // TODO: check whethere this is necessary -> if it is also make previous mutable
         let client = Client::with_options(client_options)?;
         let database = client.database(database_name);
         let collection = database.collection(collection_name);
@@ -21,10 +30,11 @@ impl Db {
     }
 
     pub async fn get_dataset(&self, id: &str) -> mongodb::error::Result<Option<Dataset>> {
-        let object_id = ObjectId::parse_str(id).map_err(|e| {
-            mongodb::error::Error::custom(format!("Invalid ObjectId: {}", e))
-        })?;
-        self.collection.find_one(doc! {"_id": object_id}, None).await
+        let object_id = ObjectId::parse_str(id)
+            .map_err(|e| mongodb::error::Error::custom(format!("Invalid ObjectId: {}", e)))?;
+        self.collection
+            .find_one(doc! {"_id": object_id}, None)
+            .await
     }
 
     pub async fn list_datasets(&self) -> mongodb::error::Result<Vec<Dataset>> {
