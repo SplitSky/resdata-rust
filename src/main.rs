34654ce7f37dbb4f154e3f2ae1e25a37878da4de
Tomasz Neska <T.Neska@Bury.gov.uk>
2025-05-01 19:15:22 +0100

    New: Added git ignore and requirements for pip integration tests

--- src/main.rs
+++ src/main.rs
@@ -31,21 +31,26 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     let config = Config::from_env();
 
-    let db = Db::new(&config.mongo_uri, &config.database_name, &config.collection_name)
-        .await
-        .expect("Failed to initialize database");
+    let db = Db::new(
+        &config.mongo_uri,
+        &config.database_name,
+        &config.collection_name,
+    )
+    .await
+    .expect("Failed to initialize database");
 
     info!("Starting server at http://127.0.0.1:8080");
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(db.clone()))
-            .route("/insert", web::post().to(handlers::handlers::insert_dataset))
+            .route(
+                "/insert",
+                web::post().to(handlers::handlers::insert_dataset),
+            )
             .service(
                 web::resource("/get/{id}").route(web::get().to(handlers::handlers::get_dataset)),
             )
-            .service(
-                web::resource("/list").route(web::get().to(handlers::handlers::list_datasets)),
-            )
+            .service(web::resource("/list").route(web::get().to(handlers::handlers::list_datasets)))
     })
     .bind("127.0.0.1:8080")?
     .run()
